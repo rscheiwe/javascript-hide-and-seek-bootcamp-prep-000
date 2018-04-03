e11b2265b1809b3f4702f9b3fa896798bf0b4df3
Richard S <rscheiwe@gmail.com>
2018-04-03 16:33:33 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,11 +14,14 @@ function increaseRankBy(n) {
   }
 }
 
-function deepestChild() {
-  var grandNode = document.querySelector('#grand-node');
-  var deepestNode = grandNode.children[0];
-  for (var i = 0; !deepestNode.children[i]; i) {
-    deepestNode = deepestNode.children[0]
+function increaseRankBy(n) {
+  const rankedLists = document.querySelectorAll('.ranked-list')
+
+  for (let i = 0, l = rankedLists.length; i < l; i++) {
+    let children = rankedLists[i].children
+
+    for (let j = 0, k = children.length; j < k; j++) {
+      children[j].innerHTML = parseInt(children[j].innerHTML) + n
+    }
   }
-  return deepestNode;
 }
